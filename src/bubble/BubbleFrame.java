0ebd9160d955a17538eced0c2ff37152d9ee7fae
HyeonProG <guswo3443@naver.com>
2024-05-01 17:01:47 +0900

    스레드 활용

---
+++ src/bubble/BubbleFrame.java
@@ -0,0 +1,9 @@
+package bubble;
+
+public class BubbleFrame {
+
+	public static void main(String[] args) {
+		
+	} // end of main
+	
+} // end of class
---
+++ src/ch08/GameTest2.java
@@ -0,0 +1,10 @@
+package ch08;
+
+public class GameTest2 {
+	
+	// 메인 작업자
+	public static void main(String[] args) {
+		new GameFrame2();
+	} // end of main
+
+} // end of class
